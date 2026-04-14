624ca81bcf6175e87285b5a0e12779ddd63f6079
cimex9 <armenak.shakaryan@proton.me>
2026-04-15 03:11:00 +0400

    Add PROJECT_ROOT helper variable to tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,9 @@
 from pathlib import Path as _Path
 
+PROJECT_ROOT = _Path(__file__).resolve().parent.parent
 TESTS_BASE_DIR = _Path(__file__).resolve().parent
 
 __all__ = [
+    'PROJECT_ROOT',
     'TESTS_BASE_DIR'
 ]
