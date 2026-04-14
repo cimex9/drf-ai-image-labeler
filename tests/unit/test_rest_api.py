5f6b151182d56d65da69dcf24f31a7e9658e2a89
cimex9 <armenak.shakaryan@proton.me>
2026-04-15 02:43:25 +0400

    Switch to pytest-style tests.

--- tests/unit/test_rest_api.py
+++ tests/unit/test_rest_api.py
@@ -1,42 +1,56 @@
+import pytest
 from django.urls import reverse
 from django.utils.dateparse import parse_datetime
-from rest_framework.test import APITestCase
-
 from app.models import Label
 
 
-class LabelsApiTest(APITestCase):
-    def setUp(self):
-        self.label1 = Label.objects.create(
-            name="Label 1",
-            description=None,
-        )
-        self.label2 = Label.objects.create(
-            name="Label 2",
-            description="Description for label 2",
-        )
+class TestLabelsApi:
+    @pytest.fixture(scope='function')
+    def labels(self, db):
+        yield {
+            'label1': Label.objects.create(
+                name="Label 1",
+                description=None,
+            ),
+            'label2': Label.objects.create(
+                name="Label 2",
+                description="Description for label 2",
+            ),
+        }
 
-    def test_get_labels(self):
+    def test_get_labels(self, api_client, labels):
         url = reverse("label-list")
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        response_data = response.data['results']
-        response_data = sorted(response_data, key=lambda x: x['id'])  # ignore the default sorting for this test case
-        self.assertEqual(len(response_data), 2)
-        self.assertEqual(response_data[0]['name'], "Label 1")
-        self.assertEqual(response_data[1]['name'], "Label 2")
-        self.assertEqual(response_data[0]['description'], None)
-        self.assertEqual(response_data[1]['description'], "Description for label 2")
-        self.assertEqual(parse_datetime(response_data[0]['created_at']), self.label1.created_at)
-        self.assertEqual(parse_datetime(response_data[1]['created_at']), self.label2.created_at)
+        response = api_client.get(url)
 
-    def test_get_label(self):
-        url = reverse("label-detail", kwargs={"pk": self.label1.pk})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data['id'], self.label1.pk)
-        self.assertEqual(response.data['name'], 'Label 1')
-        self.assertEqual(response.data['description'], None)
-        self.assertEqual(parse_datetime(response.data['created_at']), self.label1.created_at)
+        assert response.status_code == 200
+        response_data = response.data["results"]
+
+        # ignore the default sorting for this test case
+        response_data = sorted(response_data, key=lambda x: x["id"])
+
+        assert len(response_data) == 2
+
+        assert response_data[0]["name"] == "Label 1"
+        assert response_data[1]["name"] == "Label 2"
+
+        assert response_data[0]["description"] is None
+        assert response_data[1]["description"] == "Description for label 2"
+
+        label1, label2 = labels.values()
+        assert parse_datetime(response_data[0]["created_at"]) == label1.created_at
+        assert parse_datetime(response_data[1]["created_at"]) == label2.created_at
+
+    def test_get_label(self, api_client, labels):
+        label1 = labels["label1"]
+
+        url = reverse("label-detail", kwargs={"pk": label1.pk})
+        response = api_client.get(url)
+        assert response.status_code == 200
+
+        assert response.data["id"] == label1.pk
+        assert response.data["name"] == "Label 1"
+        assert response.data["description"] is None
+
+        assert parse_datetime(response.data["created_at"]) == label1.created_at
 
     # TODO: add more tests
