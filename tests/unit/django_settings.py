ca79297b60885ebe60c2e016948a17c6aa108d25
cimex9 <armenak.shakaryan@proton.me>
2026-04-15 03:33:48 +0400

    Comment fix.

--- tests/unit/django_settings.py
+++ tests/unit/django_settings.py
@@ -1,6 +1,6 @@
 """
 This file acts as DJANGO_SETTINGS_MODULE for unit tests. This file is set as
-Django settings file in the tests/pytest.ini file.
+Django settings file in the tests/unit/pytest.ini file.
 """
 from django.core.management.utils import get_random_secret_key
 
