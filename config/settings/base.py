487294c11cf85af6dd5821b18c45fb254ad5f2f7
cimex9 <armenak.shakaryan@proton.me>
2026-04-21 01:07:10 +0400

    Handle static files for `DEBUG=False` mode.

--- config/settings/base.py
+++ config/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -91,6 +92,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # media files
 STORAGES = {
